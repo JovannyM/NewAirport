0d4885cb3c70f33fe32274169f29f0d0b8df0af3
JovannyM <jovannym@mail.ru>
2021-06-02 17:29:37 +0300

    Start

---
+++ NewAirport/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace NewAirport
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
