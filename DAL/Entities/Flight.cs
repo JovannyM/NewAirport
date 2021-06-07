27a63fc720b94d82b3bf951c32ad5feb58719149
JovannyM <jovannym@mail.ru>
2021-06-08 00:59:24 +0300

    ИСПРАВИЛИ!

--- DAL/Entities/Flight.cs
+++ DAL/Entities/Flight.cs
@@ -7,12 +7,15 @@ namespace DAL.Entities
     public class Flight : BaseEntity
     {
         public Model Model { get; set; }
-        public Airplane Airplane { get; set; }
-        public Airport Airport { get; set; }
+        public virtual Airplane Airplane { get; set; }
+        public virtual Airport Airport { get; set; }
+
         [NotMapped]
         public Airport StartAirport { get; set; }
+
         [NotMapped]
         public Airport EndAirport { get; set; }
+
         public DateTime DateTime { get; set; }
         public bool IsDeparture { get; set; }
         public double? Cost { get; set; }
