c055b2673c4c444dd5c889262262ca33928a4aac
JovannyM <jovannym@mail.ru>
2021-09-07 15:16:01 +0300

    Add UI

--- NewAirport/VVM/Editer/ScheduleEditer/ScheduleEditerVM.cs
+++ NewAirport/VVM/Editer/ScheduleEditer/ScheduleEditerVM.cs
@@ -11,8 +11,37 @@ namespace NewAirport.VVM.Editer.ScheduleEditer
     public class ScheduleEditerVM : BaseVM
     {
         private ObservableCollection<Flight> _listOfFlights;
+        private ObservableCollection<Airport> _listOfAirport;
         private Flight _selectedFlight;
         private Flight _editableFlight;
+        private string _departureDateLabel = "Дата...";
+        private string _departureCityLabel = "Город...";
+
+        public string DepartureDateLabel
+        {
+            get
+            {
+                return _departureDateLabel;
+            }
+            private set
+            {
+                _departureDateLabel = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        public string DepartureCityLabel
+        {
+            get
+            {
+                return _departureCityLabel;
+            }
+            private set
+            {
+                _departureCityLabel = value;
+                OnPropertyChanged();
+            }
+        }
 
         public ObservableCollection<Flight> ListOfFlights
         {
@@ -20,6 +49,16 @@ namespace NewAirport.VVM.Editer.ScheduleEditer
             private set
             {
                 _listOfFlights = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        public ObservableCollection<Airport> ListOfAirports
+        {
+            get => _listOfAirport;
+            private set
+            {
+                _listOfAirport = value;
                 OnPropertyChanged();
             }
         }
@@ -41,6 +80,8 @@ namespace NewAirport.VVM.Editer.ScheduleEditer
             private set
             {
                 _editableFlight = value;
+                DepartureDateLabel = _editableFlight.IsDeparture ? "Время отправления" : "Время прибытия";
+                DepartureCityLabel = _editableFlight.IsDeparture ? "Город отправления" : "Город прибытия";
                 OnPropertyChanged();
             }
         }
@@ -48,12 +89,14 @@ namespace NewAirport.VVM.Editer.ScheduleEditer
         public ScheduleEditerVM()
         {
             ListOfFlights = new ObservableCollection<Flight>(DB.Flights.GetList());
-            DB.OnUpdateDbEvent += GetFlights;
+            ListOfAirports = new ObservableCollection<Airport>(DB.Airports.GetList());
+            DB.OnUpdateDbEvent += GetItems;
         }
 
-        private void GetFlights(object sender, EventArgs e)
+        private void GetItems(object sender, EventArgs e)
         {
             ListOfFlights = new ObservableCollection<Flight>(DB.Flights.GetList());
+            ListOfAirports = new ObservableCollection<Airport>(DB.Airports.GetList());
         }
         
         private RelayCommand checkAndSave;
