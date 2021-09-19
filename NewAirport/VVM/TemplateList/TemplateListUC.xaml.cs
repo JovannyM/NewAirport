029633e0d9ef7ab10c498feedf53b23fbd67b452
evgenii.manin <evgenii.manin@akvelon.com>
2021-09-19 20:37:11 +0300

    add TUC.xaml.cs

---
+++ NewAirport/VVM/TemplateList/TemplateListUC.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace NewAirport.VVM.TemplateList
+{
+    public partial class TemplateListUC : UserControl
+    {
+        public TemplateListUC()
+        {
+            InitializeComponent();
+        }
+    }
+}
